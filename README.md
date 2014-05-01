e2c29585b15b426275e4264dcbcbb8e7a94f1f26
Eric Pinto <ericpinto1985@gmail.com>
2014-05-01 11:16:43 +0200

    Add bash and folder

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ screen-layouts
 ==============
 
 A set of GNU Screen layouts made simple
+
+Usage
+-----
+
+Just add to your .bashrc or .bash\_profile the following line
+
+`. **PATH_TO_PROJECT**/screenAlias.sh`
---
+++ screenAlias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_PATH="`pwd`/`dirname \"$0\"`"
+
+# Screen aliases
+alias screen12="screen -c $SCRIPT_PATH/screenLayouts/screen1x2" # Screen 1row  x 2columns 
+alias screen21="screen -c $SCRIPT_PATH/screenLayouts/screen2x1" # Screen 2rows x 1column
+alias screen22="screen -c $SCRIPT_PATH/screenLayouts/screen2x2" # Screen 2rows x 2columns
+alias screen14="screen -c $SCRIPT_PATH/screenLayouts/screen1x4" # Screen 1row  x 4columns
+alias screen41="screen -c $SCRIPT_PATH/screenLayouts/screen4x1" # Screen 4rows x 1column
+
